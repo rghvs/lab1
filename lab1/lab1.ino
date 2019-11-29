2cb61b3b7a666b24048cc3333396d011e916e005
rghvs <56509630+rghvs@users.noreply.github.com>
2019-11-30 00:33:18 +0500

    Update lab1.ino
    Fix to DFA.

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,3 +1,4 @@
+#include "button.h"
 #define BUTTON_PIN0 9
 #define BUTTON_PIN1 10
 #define BUTTON_PIN2 11
@@ -6,92 +7,76 @@
 #define BLUE_PIN 5
 #define TIME1 250
 #define TIME2 80
+#define NUMBER_OF_BLINKS 6
 #define NUMBER_OF_COMPONENTS 3
-#define BRIGHT_VALUE 8
+#define MAX_COLOR_VALUE 255
+#define MIN_COLOR_VALUE 0
+#define NEW_COLOR_VALUE 240
 
-unsigned char redBright = BRIGHT_VALUE;
-unsigned char greenBright = BRIGHT_VALUE;
-unsigned char blueBright = BRIGHT_VALUE;
-
-char isUpButton0 = 1;
-char isUpButton1 = 1;
-char isUpButton2 = 1;
+Button buttons[] =
+{
+    new Button(BUTTON_PIN0),
+    new Button(BUTTON_PIN1),
+    new Button(BUTTON_PIN2)
+};
+char pinsAndValues[3][2] =
+{
+    { RED_PIN, MIN_COLOR_VALUE },
+    { GREEN_PIN, MIN_COLOR_VALUE },
+    { BLUE_PIN, MIN_COLOR_VALUE }
+};
+char indexPin1 = 0;
+char indexPin2 = 1;
+char state = 1;
+char isBlink = 0;
+char countBlink = 0;
 
 void setup()
 {
-    pinMode(BUTTON_PIN0, INPUT);
-    pinMode(BUTTON_PIN1, INPUT);
-    pinMode(BUTTON_PIN2, INPUT);
     pinMode(RED_PIN, OUTPUT);
     pinMode(GREEN_PIN, OUTPUT);
     pinMode(BLUE_PIN, OUTPUT);
 }
 
-void changeColorValue(int buttonPin, char *isUpButton, unsigned char *bright)
-{
-    if (digitalRead(buttonPin) == HIGH)
-    {
-        if (*isUpButton)
-        {
-            *bright += BRIGHT_VALUE;
-            *isUpButton = 0;
-        }
-    }
-    else
-        *isUpButton = 1;
-}
-
 void changeColorsValues()
 {
-    changeColorValue(BUTTON_PIN0, &isUpButton0, &redBright);
-    changeColorValue(BUTTON_PIN1, &isUpButton1, &greenBright);
-    changeColorValue(BUTTON_PIN2, &isUpButton2, &blueBright);
-}
-
-void setValueByPin(int pin, int value, int time)
-{
-    changeColorsValues();
-    analogWrite(pin, value);
-    delay(time);
-}
-
-void changeColorLEDSmoothly(int colorPin1, int colorValue1, int colorPin2, int colorValue2)
-{
-    setValueByPin(colorPin1, colorValue1, TIME1);
-    setValueByPin(colorPin2, colorValue2, TIME1);
-    changeColorsValues();
-    analogWrite(colorPin1, 0);
-}
-
-void changeColorLEDBlinking(int colorPin1, int colorValue1, int colorPin2, int colorValue2)
-{
-    int i;
-    for (i = 0; i < NUMBER_OF_COMPONENTS; ++i)
-    {
-        setValueByPin(colorPin1, colorValue1, TIME2);
-        setValueByPin(colorPin1, 0, TIME2);
-    }
-    for (i = 0; i < NUMBER_OF_COMPONENTS; ++i)
-    {
-        analogWrite(colorPin1, colorValue1);
-        setValueByPin(colorPin2, colorValue2, TIME2);
-        analogWrite(colorPin1, 0);
-        setValueByPin(colorPin2, 0, TIME2);
-    }
+    analogWrite(RED_PIN, MAX_COLOR_VALUE);
+    analogWrite(GREEN_PIN, MAX_COLOR_VALUE);
+    analogWrite(BLUE_PIN, MAX_COLOR_VALUE);
+    for (int i = 0; i < NUMBER_OF_COMPONENTS; ++i)
+        if (buttons[i].wasPressed())
+            pinsAndValues[i][1] = NEW_COLOR_VALUE - pinsAndValues[i][1];
 }
 
 void loop()
 {
-    changeColorLEDSmoothly(RED_PIN, redBright, GREEN_PIN, greenBright);
-    changeColorLEDSmoothly(GREEN_PIN, greenBright, BLUE_PIN, blueBright);
-    changeColorLEDSmoothly(BLUE_PIN, blueBright, RED_PIN, redBright);
-    analogWrite(RED_PIN, 0);
-    delay(TIME1);
     changeColorsValues();
-
-    changeColorLEDBlinking(RED_PIN, redBright, GREEN_PIN, greenBright);
-    changeColorLEDBlinking(GREEN_PIN, greenBright, BLUE_PIN, blueBright);
-    changeColorLEDBlinking(BLUE_PIN, blueBright, RED_PIN, redBright);
-    changeColorsValues();
-    delay(TIME1 - TIME2);
+    state = !state;
+    if (!isBlink && !countBlink || state)
+        analogWrite(pinsAndValues[indexPin1][0], pinsAndValues[indexPin1][1]);
+    if (!isBlink && countBlink)
+    {
+        countBlink = 0;
+        state = !state;
+    }
+    else if (state)
+    {
+        if (!isBlink || countBlink >= NUMBER_OF_BLINKS >> 1)
+            analogWrite(pinsAndValues[indexPin2][0], pinsAndValues[indexPin2][1]);
+        if (!isBlink || ++countBlink == NUMBER_OF_BLINKS)
+        {
+            if (++indexPin2 == NUMBER_OF_COMPONENTS)
+                indexPin2 = 0;
+            if (++indexPin1 == NUMBER_OF_COMPONENTS)
+            {
+                indexPin1 = 0;
+                isBlink = !isBlink;
+                if (isBlink)
+                    delay(TIME1 - TIME2);
+            }
+            if (isBlink)
+                countBlink = 0;
+        }
+    }
+    delay(isBlink ? TIME2 : TIME1);
 }
